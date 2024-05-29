5fcc58519a39931d79691dce6327006482eea684
auzanassdq <auzanassidqi@gmail.com>
2024-05-29 08:14:39 +0700

    init commit

--- src/app/cat/[id]/page.tsx
+++ src/app/cat/[id]/page.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
 
-async function getData (id: string) {
-  const response = await fetch(`https://api.thecatapi.com/v1/breeds/${id}`)
-  return response.json()
+async function getData(id: string) {
+  const response = await fetch(`https://api.thecatapi.com/v1/breeds/${id}`);
+  return response.json();
 }
 
-export default async function DetailCat ({params}: {params: {id: string}}) {
+export default async function DetailCat({
+  params,
+}: {
+  params: { id: string };
+}) {
   let linkImage = "https://cdn2.thecatapi.com/images";
-  const cat = await getData(params.id)
+  const cat = await getData(params.id);
 
   return (
     <div>
       <h1 className="text-6xl font-bold">{cat.name}</h1>
-      <Image 
+      <Image
         src={`${linkImage}/${cat.reference_image_id}.jpg`}
         alt={cat.name}
         width={800}
@@ -20,5 +24,5 @@ export default async function DetailCat ({params}: {params: {id: string}}) {
       />
       <p>{cat.description}</p>
     </div>
-  )  
+  );
 }
--- src/app/cat/page.tsx
+++ src/app/cat/page.tsx
@@ -7,24 +7,22 @@ async function getData() {
 }
 
 export default async function CatPage() {
-  // await new Promise(() => setTimeout(() => console.log("tes"), 5000)) 
-
   let linkImage = "https://cdn2.thecatapi.com/images";
   const cats = await getData();
 
   return (
-    <section className="flex flex-row gap-4 flex-wrap">
+    <section className="flex flex-row flex-wrap gap-4">
       {cats.map((item: any) => (
         <div className="w-52 border-2 border-gray-900" key={item.id}>
           <Link href={`/cat/${item.id}`}>
-          <Image
-            src={`${linkImage}/${item.reference_image_id}.jpg`}
-            alt={item.name}
-            width={800}
-            height={100}
-            className="h-40 object-cover"
-          />
-          <h2 className="text-center text-xl font-bold">{item.name}</h2>
+            <Image
+              src={`${linkImage}/${item.reference_image_id}.jpg`}
+              alt={item.name}
+              width={800}
+              height={100}
+              className="h-40 object-cover"
+            />
+            <h2 className="text-center text-xl font-bold">{item.name}</h2>
           </Link>
         </div>
       ))}
